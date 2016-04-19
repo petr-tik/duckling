a6181109ff2750ebe110f1a5206e8766d43120a3
petr-tik <tikilyaynen@gmail.com>
2016-04-19 22:02:01 +0100

    added corpus for numbers up to 100. These building blocks are enough to get any number between 1..99

---
+++ resources/duckling/corpus/de.numbers.clj
@@ -0,0 +1,58 @@
+(
+; context map
+ {}
+ 
+ "eins"
+ (number 1)
+
+ "zwei"
+ "zwo"
+ (number 2)
+
+ "drei"
+ (number 3)
+
+ "vier"
+ (number 4)
+
+ "funf"
+ "fuenf"
+ "fünf"
+ (number 5)
+
+ "sechs"
+ (number 6)
+
+ "sieben"
+ (number 7)
+
+ "acht"
+ (number 8)
+
+ "neun"
+ (number 9)
+
+ "zehn"
+ (number 10)
+
+ "elf"
+ (number 11)
+
+ "zwolf"
+ "zwoelf"
+ "zwölf"
+ (number 12)
+
+ "zwanzig"
+ (number 20)
+
+ "sechszig"
+ (number 60)
+
+ "siebzig"
+ (number 70)
+
+ "hundert"
+ (number 100)
+
+)
