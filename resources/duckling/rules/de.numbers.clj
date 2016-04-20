eee10a2a971ff084980527db72cab0f1e6ea53fa
petr-tik <tikilyaynen@gmail.com>
2016-04-20 01:06:29 +0100

    added zero to corpus and rules for 0..12, 16, 17
    the rule currently recognises just the written form of the numbers, but once this is done the values can be combined to get any value between 0..100

---
+++ resources/duckling/rules/de.numbers.clj
@@ -0,0 +1,13 @@
+(
+ ;;
+ ;; Integers
+ ;;
+
+ "number (0..12, 16, 17)"
+ #"(?i)(null|eins|zw(ei|o)|drei|vier|f(ue|u|ü)nf|sechs|sieben|acht|neun|zehn|elf|zw(oe|o|ö)lf|sechzehn|siebzehn)"
+ {:dim :number
+  :integer true
+  :value (get {"null" 0 "eins" 1 "zwei" 2 "zwo" 2 "drei" 3 "vier" 4 "funf" 5 "fuenf" 5 "fünf" 5 "sechs" 6 "sieben" 7 "acht" 8 "neun" 9 "zehn" 10 "elf" 11 "zwolf" 12 "zwoelf" 12 "zwölf" 12 "sechzehn" 16 "siebzehn" 17}
+(-> %1 :groups first .toLowerCase))}
+
+)
