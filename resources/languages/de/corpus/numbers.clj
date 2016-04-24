1b610eb7ab4982c76b1832af60e8cec9206868d5
petr-tik <petr-tik@users.noreply.github.com>
2016-04-25 00:46:43 +0100

    copied changes from german_number
    saving progress in case the merge goes badly

---
+++ resources/languages/de/corpus/numbers.clj
@@ -0,0 +1,137 @@
+(
+; context map
+ {}
+
+;;
+;; Integers
+;; 
+
+ "0"
+ "null"
+ (number 0)
+
+ "1"
+ "eine"
+ "einer"
+ "eins"
+ (number 1)
+
+ "2"
+ "zwei"
+ "zwo"
+ (number 2)
+
+ "3"
+ "drei"
+ (number 3)
+
+ "4"
+ "vier"
+ (number 4)
+
+ "5"
+ "funf"
+ "fuenf"
+ "fünf"
+ (number 5)
+
+ "6"
+ "06"
+ "sechs"
+ (number 6)
+
+ "07"
+ "7"
+ "007" ; tribute to Bond, James Bond :-)
+ "sieben"
+ (number 7)
+
+ "8"
+ "08"
+ "acht"
+ (number 8)
+
+ "9"
+ "09"
+ "neun"
+ (number 9)
+
+ "10"
+ "zehn"
+ (number 10)
+
+ "11"
+ "elf"
+ (number 11)
+
+ "12"
+ "zwolf"
+ "zwoelf"
+ "zwölf"
+ (number 12)
+
+ "13"
+ "dreizehn"
+ (number 13)
+
+ "16"
+ "sechzehn"
+ (number 16)
+
+ "17"
+ "siebzehn"
+ (number 17)
+
+ "20"
+ "zwanzig"
+ (number 20)
+
+ "28"
+ "achtundzwanzig"
+ (number 28)
+
+ "30"
+ "dreissig"
+ "dreiβig"
+ (number 30)
+
+ "40"
+ "vierzig"
+ (number 40)
+
+ "60"
+ "sechzig"
+ (number 60)
+ 
+ "70"
+ "siebzig"
+ (number 70)
+
+ "100"
+ "hundert"
+ (number 100)
+
+;;
+;; Ordinal numbers
+;;
+
+ "erste"
+ "erster"
+ "erstes"
+ (ordinal 1)
+
+ "dritte"
+ "dritter"
+ "drittes"
+ (ordinal 3)
+
+ "siebtes"
+ "siebter"
+ (ordinal 7)
+
+ "achtes"
+ "achter"
+ (ordinal 8)
+
+)
+
